cdf1360d8dd2f2a4fa792129d15aa1e780973b05
Dyan Carra <dyancarra@gmail.com>
2017-01-12 21:17:39 -0200

    minor, gitignore

--- controllers/mock_controller.rb
+++ controllers/mock_controller.rb
@@ -1,7 +1,23 @@
 class MockController < ApplicationController
 
   get "/mock" do
-    "mock"
+    "instructions"
+  end
+
+  get "/mock/error" do
+    content_type :json
+    { message: "endpoint not found." }.to_json
+  end
+
+  get "/mock/:name" do
+    content_type :json
+
+    begin
+      endpoints = YAML.load_file("config/endpoints.yml")
+      return File.read("endpoints_files/#{endpoints[params["name"]]["file"]}")
+    rescue
+      redirect "/mock/error"
+    end
   end
 
 end
--- controllers/vladimir_controller.rb
+++ controllers/vladimir_controller.rb
@@ -1,17 +1,3 @@
-# EXAMPLE 1
-#
-# get "/first" do
-# content_type :json
-# File.read("endpoints_files/products.json")
-# end
-
-# EXAMPLE 2
-#
-# get "/first" do
-# configs = YAML.load_file("config/endpoints.yml")
-# configs.inspect
-# end
-
 class VladimirController < ApplicationController
 
   before do
